e967e92e97ab4be0e0bb8434ae6b8676dfb43753
Morlok <nikitaillarionov2018@gmail.com>
2024-09-30 23:23:09 +0300

    Added previously worked on, repo to the git. Initial commit

---
+++ Learning PHP from Online-Course/index.php
@@ -0,0 +1,44 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+// echo "<b>Hello</b> world" ;
+//echo "<br><b>Hello</b> world" ;
+
+
+//echo "<br>\"\"";
+//dasdscfa
+//asfa
+/*sdgsdgbzdfgzdr*/
+#xdvbz
+/*zdfb
+ * zdfb
+ * xf
+ * zdfb
+ * zdfb
+ * */
+
+
+
+$number = 0;
+echo $number;
+$number ++;
+echo $number;
+
+echo "<br>";
+echo "Var:" . $number . $number;
+$var = false;
+
+echo "<br>";echo "<br>";
+$rand = mt_rand(1 , 20);
+echo $rand;
+?>
+</body>
+</html>
