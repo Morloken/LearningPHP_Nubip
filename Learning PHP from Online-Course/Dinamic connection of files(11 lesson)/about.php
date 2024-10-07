a54a9d291bcbc9f5f9d7b2eb3c976c921c3ab00f
Illarionov Mykyta <nikitaillarionov2018@gmail.com>
2024-10-07 12:54:49 +0300

    working on lesson 12

--- Learning PHP from Online-Course/Dinamic connection of files(11 lesson)/about.php
+++ Learning PHP from Online-Course/Dinamic connection of files(11 lesson)/about.php
@@ -6,7 +6,7 @@ require "blocks/header.php";
 <div class="container">
     <h1>About us</h1>
 
-    <form action="check_post.php" method="post">
+    <form action="blocks/check_post.php" method="post">
         <input type="text" name="username" placeholder="Enter name" class="form-control"><br>
         <input type="email" name="email" placeholder="Enter email" class="form-control"><br>
         <input type="password" name="password" placeholder="Enter password" class="form-control"><br>
--- Learning PHP from Online-Course/Dinamic connection of files(11 lesson)/blocks/check_post.php
+++ Learning PHP from Online-Course/Dinamic connection of files(11 lesson)/blocks/check_post.php
@@ -1,1 +1,32 @@
     <?php
+//    print_r($_POST);
+//        $list = [4,5,6,7];
+//        print_r($list);
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password  = $_POST['password'];
+    $message = $_POST['message'];
+
+    if(trim($name) == ''){
+        echo "Name is required";
+    }elseif(strlen(trim($name)) < 3){
+        echo "Name must be at least 3 characters";
+    }
+
+    else if(trim($email) == ''){
+        echo "Email is required";
+    }else if(trim($password) == ''){
+        echo "Password is required";
+    }
+    else if(trim($message) == ''){
+        echo "Message is required";
+    }
+
+
+
+    else{
+        echo "<h1> All your data </h1>";
+        foreach ($_POST as $key => $value) {
+            echo "<p>Key: $key, value: $value</p>";
+        }
+    }
