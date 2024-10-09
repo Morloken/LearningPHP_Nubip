3f28de68c70c234764dfa0a0b4ec1b86c2312455
Morlok <nikitaillarionov2018@gmail.com>
2024-10-10 00:44:19 +0300

    completed 13 lesson, added styles to main page

--- Learning PHP from Online-Course/Dinamic connection of files(11 lesson)/blocks/header.php
+++ Learning PHP from Online-Course/Dinamic connection of files(11 lesson)/blocks/header.php
@@ -5,6 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $title; ?></title>
     <title><?=$title?></title>
+    <style>
+
+        body{
+            background: #fff;
+            color: black;
+            margin: 20px;
+        }
+    </style>
 
 
     <link rel="stylesheet"  href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
--- Learning PHP from Online-Course/Dinamic connection of files(11 lesson)/index.php
+++ Learning PHP from Online-Course/Dinamic connection of files(11 lesson)/index.php
@@ -5,9 +5,37 @@ require "blocks/header.php";
 #   include better
 # include_once Only one time
 ?>
-<h1>Home page</h1>
+<style>
+    body{
+        background: #050505;
+        color: white;
+    }
+    header, footer{
+        display:flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        gap: 1vw;
+    }
+    .zagolovok{
+        text-align: center;
+    }
+</style>
+<h1 class="zagolovok">Home page</h1>
 
 
 <?php
+date_default_timezone_set('Europe/Kiev');
+// echo date('t F');
+echo date('m-l Час: H:i:s    ', time() + 1000)
+    .'<br>';
+echo time()
+    .'<br>';
+echo date('m-l Час: H:i:s    ', strtotime('now'))
+    .'<br>';
+echo date('m-l Час: H:i:s    ', strtotime('+1 hour'))
+    .'<br>';
+echo date('m-l Час: H:i:s    ', strtotime('next Day'));
+
 require "blocks/footer.php";
 ?>
