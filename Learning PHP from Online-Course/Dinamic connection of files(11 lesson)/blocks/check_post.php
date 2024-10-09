ad459b3017787f94348c40c52b647cef7ed2980c
Morlok <nikitaillarionov2018@gmail.com>
2024-10-10 00:21:27 +0300

    corrected the mistake

--- Learning PHP from Online-Course/Dinamic connection of files(11 lesson)/blocks/check_post.php
+++ Learning PHP from Online-Course/Dinamic connection of files(11 lesson)/blocks/check_post.php
@@ -2,31 +2,26 @@
 //    print_r($_POST);
 //        $list = [4,5,6,7];
 //        print_r($list);
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $password  = $_POST['password'];
-    $message = $_POST['message'];
 
-    if(trim($name) == ''){
+// Use isset() to check if the keys exist in the $_POST array
+    $username = isset($_POST['username']) ? $_POST['username'] : null;
+$email = isset($_POST['email']) ? $_POST['email'] : null;
+$password = isset($_POST['password']) ? $_POST['password'] : null;
+$message = isset($_POST['message']) ? $_POST['message'] : null;
+
+    if (trim($username) == '') {
         echo "Name is required";
-    }elseif(strlen(trim($name)) < 3){
-        echo "Name must be at least 3 characters";
+} elseif (strlen(trim($username)) < 3) {
+    echo "Name must be at least 3 characters";
+} elseif (trim($email) == '') {
+    echo "Email is required";
+} elseif (trim($password) == '') {
+    echo "Password is required";
+} elseif (trim($message) == '') {
+    echo "Message is required";
+} else {
+    echo "<h1> All your data </h1>";
+    foreach ($_POST as $key => $value) {
+        echo "<p>Key: $key, value: $value</p>";
     }
-
-    else if(trim($email) == ''){
-        echo "Email is required";
-    }else if(trim($password) == ''){
-        echo "Password is required";
-    }
-    else if(trim($message) == ''){
-        echo "Message is required";
-    }
-
-
-
-    else{
-        echo "<h1> All your data </h1>";
-        foreach ($_POST as $key => $value) {
-            echo "<p>Key: $key, value: $value</p>";
-        }
-    }
+}
