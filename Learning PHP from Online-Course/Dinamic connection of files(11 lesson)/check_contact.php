27b1c17e448ab7fa29875d4a200e2acc6f239dda
Illarionov Mykyta <nikitaillarionov2018@gmail.com>
2024-10-21 12:17:35 +0300

    working on lesson 19

--- Learning PHP from Online-Course/Dinamic connection of files(11 lesson)/check_contact.php
+++ Learning PHP from Online-Course/Dinamic connection of files(11 lesson)/check_contact.php
@@ -1,1 +1,15 @@
 <?php
+    session_start();
+
+    $username = htmlspecialchars($_POST["username"]);
+    $email = htmlspecialchars($_POST["email"]);
+    $subject = htmlspecialchars($_POST["subject"]);
+    $message = htmlspecialchars($_POST["message"]);
+
+    $_SESSION['username'] = $username;
+    $_SESSION['email'] = $email;
+    $_SESSION['subject'] = $subject;
+    $_SESSION['message'] = $message;
+
+
+
--- Learning PHP from Online-Course/Dinamic connection of files(11 lesson)/contacts.php
+++ Learning PHP from Online-Course/Dinamic connection of files(11 lesson)/contacts.php
@@ -4,6 +4,10 @@
 require "blocks/header.php";
 
 $title = "Contacts";
+//
+//if(){
+//
+//}
 
 ?>
 <style>
@@ -24,7 +28,7 @@ $title = "Contacts";
 </style>
 <h1 class="zagolovok"><?=$title?></h1>
 <form action="check_contact.php" method="post">
-    <input type="text" name="username" placeholder="Input your name" class="form-control">
+    <input type="text" name="username" placeholder="Input your name" class="form-control" value = "<?=$_SESSION['username']?>">
     <input type="email" name="email" placeholder="Input your email" class="form-control">
     <input type="text" name="subject" placeholder="Input your subject" class="form-control">
     <textarea name="message" class="form-control" placeholder="Input your message"></textarea>
