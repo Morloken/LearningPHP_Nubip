e155082bdace113b2f1de809155dda481d64119b
Illarionov Mykyta <nikitaillarionov2018@gmail.com>
2024-10-30 11:07:52 +0200

    finished lesson 19

--- Learning PHP from Online-Course/Dinamic connection of files(11 lesson)/check_contact.php
+++ Learning PHP from Online-Course/Dinamic connection of files(11 lesson)/check_contact.php
@@ -1,6 +1,25 @@
 <?php
     session_start();
 
+
+
+    unset($_SESSION['username']);
+    unset($_SESSION['email']);
+    unset($_SESSION['subject']);
+    unset($_SESSION['message']);
+
+    unset($_SESSION['error_username']);
+    unset($_SESSION['error_email']);
+    unset($_SESSION['error_subject']);
+    unset($_SESSION['error_message']);
+
+
+    function  redirect()
+    {
+        header("location:contact.php");
+        exit();
+    }
+
     $username = htmlspecialchars($_POST["username"]);
     $email = htmlspecialchars($_POST["email"]);
     $subject = htmlspecialchars($_POST["subject"]);
@@ -11,5 +30,30 @@
     $_SESSION['subject'] = $subject;
     $_SESSION['message'] = $message;
 
+    if(strlen($username) <= 1 || strlen($username) > 30){
+       $_SESSION['error_username'] = "Username must be between 1 and 30 characters";
+        redirect();
+    }
+    else if(strlen($email) <= 1 || strlen($email) > 50){
+        $_SESSION['error_email'] = "Email must be between 1 and 50 characters";
+        redirect();
+    }
+    else if(strlen($subject) <= 1 || strlen($subject) > 50){
+        $_SESSION['error_subject'] = "Subject must be between 1 and 50 characters";
+        redirect();
+    }
+    else if(strlen($message) <= 1 || strlen($message) > 500){
+        $_SESSION['error_message'] = "Message must be between 1 and 500 characters";
+        redirect();
+    }
+else{
+    $subject = "=?utf-8?B?".base64_encode($subject)."?=";
+    $headers = "From: " . $email . "\r\n" . "Reply-To: " . $email .
+        "\r\n" . "X-Mailer: PHP/" . phpversion() . "\r\n";
+    mail("test@gmail.com", $subject, $message, $headers);
+
+    $_SESSION['message'] = "Your message has been sent!";
+    redirect();
+}
 
 
--- Learning PHP from Online-Course/Dinamic connection of files(11 lesson)/contacts.php
+++ Learning PHP from Online-Course/Dinamic connection of files(11 lesson)/contacts.php
@@ -4,10 +4,24 @@
 require "blocks/header.php";
 
 $title = "Contacts";
-//
-//if(){
-//
-//}
+
+if(isset($_SESSION['username'])) $username = $_SESSION['username'];
+else $username = "";
+
+if(isset($_SESSION['error_username'])) $error_username = $_SESSION['error_username'];
+else $error_username = "";
+
+if(isset($_SESSION['error_email'])) $error_email = $_SESSION['error_email'];
+else $error_email  = "";
+
+if(isset($_SESSION['error_subject'])) $error_subject = $_SESSION['error_subject'];
+else $error_subject  = "";
+
+if(isset($_SESSION['error_message'])) $error_message = $_SESSION['error_message'];
+else $error_message  = "";
+
+
+
 
 ?>
 <style>
@@ -27,11 +41,16 @@ $title = "Contacts";
     }
 </style>
 <h1 class="zagolovok"><?=$title?></h1>
+
 <form action="check_contact.php" method="post">
     <input type="text" name="username" placeholder="Input your name" class="form-control" value = "<?=$_SESSION['username']?>">
-    <input type="email" name="email" placeholder="Input your email" class="form-control">
-    <input type="text" name="subject" placeholder="Input your subject" class="form-control">
-    <textarea name="message" class="form-control" placeholder="Input your message"></textarea>
+    <div class="text-danger"><?=$error_username?></div>
+    <input type="email" name="email" placeholder="Input your email" class="form-control" value = "<?=$_SESSION['email']?>">
+    <div class="text-danger"><?=$error_email?></div>
+    <input type="text" name="subject" placeholder="Input your subject" class="form-control" value = "<?=$_SESSION['subject']?>">
+    <div class="text-danger"><?=$error_subject?></div>
+    <textarea name="message" class="form-control" placeholder="Input your message"><?=$_SESSION['message']?></textarea>
+    <div class="text-danger"><?=$error_message?></div>
     <button type="submit" class="btn btn-success">Send</button>
 
 </form>
